a3823c1674542dc9ae363e5f92885103a03c269c
Tim <treehousetim@gmail.com>
2022-07-28 17:42:56 -0500

    formatting

--- src/Classes/Tags.php
+++ src/Classes/Tags.php
@@ -2,36 +2,28 @@
 
 class Tags extends \treehousetim\ActiveCampaign\Connector
 {
-
-
 	public function get($tag_id)
 	{
 		return $this->request('GET', 'tags/' . strval($tag_id));
 	}
-
 
 	public function all( $search = null )
 	{
 		return $this->request('GET', 'tags', ['search' => $search] );
 	}
 
-
 	public function create( array $params )
 	{
 		return $this->request( 'POST', 'tags', ['tag' => $params] );
 	}
-
 
 	public function update( $tag_id, $params )
 	{
 		return $this->request('PUT', 'tags/' . strval($tag_id), ['tag' => $params]);
 	}
 
-
 	public function delete( $tag_id )
 	{
 		return $this->request('DELETE', 'tags/' . strval($tag_id));
 	}
-
-
 }
